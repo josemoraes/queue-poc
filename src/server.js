43c3415b721f9332d8c9d5c060d81a0c2d3d41f3
Jose Carlos De Moraes Filho <jose.filho@sistemafiep.org.br>
2020-08-11 23:43:37 -0300

    Added bull board to see the history of job executions

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import express from "express";
 import UserController from "./app/controllers/UserController";
+import BullBoard from "bull-board";
+import Queue from "./app/lib/Queue";
 
 const app = express();
-app.use(express.json());
+BullBoard.setQueues(Queue.queues.map((queue) => queue.bull));
 
+app.use(express.json());
+app.use("/admin/queues", BullBoard.UI);
 app.post("/users", UserController.store);
 
 app.listen(3333, () => {
